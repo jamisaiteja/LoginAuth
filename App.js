408b7c32531b072fc4d55291370d8a6647880e1d
jamisaiteja <jamisaiteja666@gmail.com>
2023-07-30 23:38:58 +0530

    app.jschange

--- App.js
+++ App.js
@@ -18,7 +18,8 @@ const port = process.env.PORT || 8009;
 //connection b/w frontend and backend
 app.use(express.json()); //frontend data is passed in json format to backend
 app.use(cookieParser());
-app.use(cors()); //  Integration between frontend and backend (because frontend(3000) and backend(8009) runs in diff ports)
+//app.use(cors()); //  Integration between frontend and backend (because frontend(3000) and backend(8009) runs in diff ports)
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(router);
 
 app.listen(port,()=>{
